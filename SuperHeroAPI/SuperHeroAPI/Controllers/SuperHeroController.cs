476ebe46f1d26b52e750ea1c7e86b66646f97e82
Shuajb Bunjaku <bunjakushuajb@gmail.com>
2024-09-29 14:35:32 +0200

    Krijimi i klases dhe controllerit te pare

---
+++ SuperHeroAPI/SuperHeroAPI/Controllers/SuperHeroController.cs
@@ -0,0 +1,11 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace SuperHeroAPI.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class SuperHeroController : ControllerBase
+    {
+    }
+}
