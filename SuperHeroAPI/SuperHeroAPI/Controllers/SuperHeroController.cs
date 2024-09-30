51655f772df5bc4583057454ad72faecc3192498
Shuajb Bunjaku <bunjakushuajb@gmail.com>
2024-09-30 14:54:57 +0200

    Lidhja me databaze dhe krijimi i HttpGet

--- SuperHeroAPI/SuperHeroAPI/Controllers/SuperHeroController.cs
+++ SuperHeroAPI/SuperHeroAPI/Controllers/SuperHeroController.cs
@@ -1,5 +1,8 @@
 ﻿using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using SuperHeroAPI.Data;
+using SuperHeroAPI.Entities;
 
 namespace SuperHeroAPI.Controllers
 {
@@ -7,5 +10,34 @@ namespace SuperHeroAPI.Controllers
     [ApiController]
     public class SuperHeroController : ControllerBase
     {
+        private readonly DataContext _context;
+
+        public SuperHeroController(DataContext context)
+        {
+            _context = context;
+        }
+
+        [HttpGet]
+        public async Task<ActionResult<List<SuperHero>>> GetAllHeroes()
+        {
+            var heroes = await _context.SuperHeroes.ToListAsync();
+
+            return Ok(heroes);
+        }
+
+        [HttpGet("{id}")]
+       
+        public async Task<ActionResult<List<SuperHero>>> GetHero(int id)
+        {
+            var hero = await _context.SuperHeroes.FindAsync(id);
+            if (hero is null)
+                return BadRequest("Hero not found");
+
+            return Ok(hero);
+        }
+
+
+
+
     }
 }
