3ff1e5376192b94111d61594450770203dc53caf
Shuajb Bunjaku <bunjakushuajb@gmail.com>
2024-09-30 15:25:17 +0200

    Shtimi i [HttpPost] dhe [HttpGet]

--- SuperHeroAPI/SuperHeroAPI/Controllers/SuperHeroController.cs
+++ SuperHeroAPI/SuperHeroAPI/Controllers/SuperHeroController.cs
@@ -26,12 +26,39 @@ namespace SuperHeroAPI.Controllers
         }
 
         [HttpGet("{id}")]
-       
-        public async Task<ActionResult<List<SuperHero>>> GetHero(int id)
+
+        public async Task<ActionResult<SuperHero>> GetHero(int id)
         {
             var hero = await _context.SuperHeroes.FindAsync(id);
             if (hero is null)
                 return BadRequest("Hero not found");
+
+            return Ok(hero);
+        }
+
+        [HttpPost]
+
+        public async Task<ActionResult<List<SuperHero>>> AddHero(SuperHero hero)
+        {
+            _context.SuperHeroes.Add(hero);
+            await _context.SaveChangesAsync();
+            return Ok(hero);
+        }
+
+        [HttpPut]
+
+        public async Task<ActionResult<List<SuperHero>>> UpdateHero(SuperHero hero)
+        {
+            var dbHero = await _context.SuperHeroes.FindAsync(hero.Id);
+            if (dbHero is null)
+                return BadRequest("Hero not found");
+
+            dbHero.Name = hero.Name;
+            dbHero.FirstName = hero.FirstName;
+            dbHero.LastName = hero.LastName;
+            dbHero.Place = hero.Place;
+
+            await _context.SaveChangesAsync();
 
             return Ok(hero);
         }
